858844b71afe695fb70fcab58606abd6cab56cf4
Meghan O'Halloran <meghaneohalloran@gmail.comuser.name meghanohallorangit pushcode>
2025-02-24 20:40:16 -0700

    removing ignored files

---
+++ db/seed.cjs
@@ -0,0 +1,57 @@
+const client = require('./client.cjs');
+
+const dropTables = async() => {
+try {
+
+await client.query(`
+  DROP TABLE IF EXISTS customers; 
+  DROP TABLE IF EXISTS restaurants; 
+  `);
+} catch(err) {
+
+}
+  console.log('drop tables');
+}
+
+const createTables = async () => {
+
+  try {
+    await client.query(`
+      CREATE TABLE customers (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(30) NOT NULL
+      );
+  
+      CREATE TABLE restaurants (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(30) NOT NULL
+
+    CREATE TABLE reservations  (
+      id SERIAL PRIMARY KEY,
+      date DATE NOT NULL
+       party_count INTEGER NOT NULL
+      restaurant_id INTEGER REFERENCES
+  );
+
+      );
+    `);
+    console.log('create tables');
+  } catch (err) {
+    console.log(err);
+  }
+  
+}
+const syncAndSeed = async() => {
+
+  await client.connect();
+  console.log('connected to the db');
+
+  await createTables();
+
+
+  await client.end();
+  console.log('disconnect db');
+}
+
+syncAndSeed();
+
